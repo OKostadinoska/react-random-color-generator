e204a89028a82472ca27b6f90a34b2ef46aa8fe4
Olivera Kostadinoska <kostadinoskaolivera@outlook.com>
2022-01-18 12:31:45 +0100

    Implementation

---
+++ src/Button.js
@@ -0,0 +1,37 @@
+// Import library from npm
+import randomColor from 'randomcolor';
+// Import React
+import React, { useState } from 'react';
+
+// Create a component
+function Button() {
+  // Create a useState variable with a String value
+  const [hex, setHex] = useState('#78d3d2');
+  // Connect the setHex to the color
+  const randomizedHex = function (tone) {
+    const color = randomColor({ hue: tone });
+    setHex(color);
+  };
+
+  return (
+    <>
+      {/* Show state variable value on the page */}
+      <div
+        className="App"
+        style={{
+          backgroundColor: hex,
+          zoom: 2,
+        }}
+      >
+        {/* Show state variable value on page */}
+        <h1 className="App-header">Generated Color:{hex}</h1>
+        {/* Update state variable */}
+        <button className="generateButton" onClick={randomizedHex}>
+          Generate
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default Button;
