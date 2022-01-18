7979c277e7443c46cc05d20376567ff7def84f8a
Olivera Kostadinoska <kostadinoskaolivera@outlook.com>
2022-01-18 13:18:48 +0100

    Added <p>

--- src/Button.js
+++ src/Button.js
@@ -24,7 +24,10 @@ function Button() {
         }}
       >
         {/* Show state variable value on page */}
-        <div className="App-header">Generated Color:{hex}</div>
+        <div className="App-header">
+          <p>Generated Color: background color hex code</p>
+          {hex}
+        </div>
         {/* Update state variable */}
         <button className="generateButton" onClick={randomizedHex}>
           Generate
